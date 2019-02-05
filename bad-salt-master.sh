6465f790dafe894282351d3b48809423b82bd0ae
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2019-02-05 15:15:18 -0500

    Update bad-salt-master.sh

--- bad-salt-master.sh
+++ bad-salt-master.sh
@@ -3,3 +3,4 @@ wget -O - https://repo.saltstack.com/apt/debian/9/amd64/latest/SALTSTACK-GPG-KEY
 sudo echo "deb http://repo.saltstack.com/apt/debian/9/amd64/latest stretch main" > /etc/apt/sources.list.d/saltstack.list
 sudo apt update
 sudo apt install salt-master
+wget -O - https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-master.conf > /etc/salt/master
